3fe1b6a2ce125d7038c1bfd9ecc2470afc46560c
Aja Izim <optimiziaa@gmail.com>
2023-12-23 11:03:32 -0800

    check

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -7,11 +7,11 @@
  * @argv: array that points to the arguments
  * Return: 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
 	int cents, mincoin = 0;
 
-	if (argc > 2 || argc == 1)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
