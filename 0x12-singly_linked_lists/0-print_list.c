2d0392fe28b5dd11bb3f9ebe1e5fe47f9dcc33b6
Your Name <optimiziaa@gmail.com>
2024-02-05 05:45:33 -0800

    Task 0. 0-print_list.c: A function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,8 +15,8 @@ size_t print_list(const list_t *h)
 
 	while (h)
 	{
-		if (h->strr == NULL)
-			ptint("[0] (nil)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
 
 		else
 			printf("[%d] %s\n", h->len, h->str);
