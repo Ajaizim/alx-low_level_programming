cdcfe2ee3b87833cc6eb82fface9cfa0c71fec71
Your Name <optimiziaa@gmail.com>
2024-02-25 20:03:54 -0800

    0. Print list: A function that prints all the elements of a listint_t list.

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,12 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stdlib.>
+#include <stdlib.h>
+
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}	listint_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
